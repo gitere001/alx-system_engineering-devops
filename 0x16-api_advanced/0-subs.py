2dd2e33bf1b91b92530dc8c5ccee1498145c1f0d
James gitere <sasmuypwa2019.com>
2024-05-08 12:47:14 +0300

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,19 +10,15 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given
-    subreddit.
-
-    :param subreddit: The name of the subreddit.
-    :return: The number of subscribers (integer). Returns 0 if the subreddit
-    is invalid or not found.
+    Function that queries the Reddit API
+    - If not a valid subreddit, return 0.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'user-agent': 'custom'}
-    response = requests.get(url, headers=headers)
-    data = response.json()
-    if 'data' in data and 'subscribers' in data['data']:
-        subscribers = data['data']['subscribers']
-        return subscribers
+    response = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
+
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
     else:
         return 0
